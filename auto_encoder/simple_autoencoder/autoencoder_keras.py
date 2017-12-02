13951ef7b1d1adda5368fc343ce75ef06facd09a
Shuvendu Bikash <bikash11roy@gmail.com>
2017-12-03 00:57:32 +0600

    training in keras

--- auto_encoder/simple_autoencoder/autoencoder_keras.py
+++ auto_encoder/simple_autoencoder/autoencoder_keras.py
@@ -40,3 +40,31 @@ x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
 print(x_train.shape)
 print(x_test.shape)
 
+
+autoencoder.fit(x_train, x_train, epochs=50, batch_size=256, shuffle=True, validation_data=(x_test, x_test))
+
+# encode and decode some digits
+# note that we take them from the *test* set
+encoded_imgs = encoder.predict(x_test)
+decoded_imgs = decoder.predict(encoded_imgs)
+
+# use Matplotlib (don't ask)
+import matplotlib.pyplot as plt
+
+n = 10  # how many digits we will display
+plt.figure(figsize=(20, 4))
+for i in range(n):
+    # display original
+    ax = plt.subplot(2, n, i + 1)
+    plt.imshow(x_test[i].reshape(28, 28))
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
+    # display reconstruction
+    ax = plt.subplot(2, n, i + 1 + n)
+    plt.imshow(decoded_imgs[i].reshape(28, 28))
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+plt.show()
