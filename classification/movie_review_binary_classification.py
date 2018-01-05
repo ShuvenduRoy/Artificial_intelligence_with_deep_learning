263eed146c94ca0605e76a0af0b75ae22b6f8525
Shuvendu Bikash <bikash11roy@gmail.com>
2018-01-05 07:51:00 +0600

    preprocessing the movie data
    Signed-off-by: Shuvendu Bikash <bikash11roy@gmail.com>

---
+++ classification/movie_review_binary_classification.py
@@ -0,0 +1,19 @@
+from keras.datasets import imdb
+(train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words=10000)
+
+word_index = imdb.get_word_index()
+
+reverse_word_index = dict([(value, key) for (key, value) in word_index.items()])
+decoded_review = ' '.join([reverse_word_index.get(i - 3, '?') for i in train_data[0]])
+
+import numpy as np
+def vectorize_sequences(sequences, dimension=10000):
+    results = np.zeros((len(sequences), dimension))
+    for i, sequence in enumerate(sequences):
+        results[i, sequence] = 1.
+    return results
+x_train = vectorize_sequences(train_data)
+x_test = vectorize_sequences(test_data)
+
+y_train = np.asarray(train_labels).astype('float32')
+y_test = np.asarray(test_labels).astype('float32')
