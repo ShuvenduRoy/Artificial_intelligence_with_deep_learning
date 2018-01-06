c341b46f9bb629d9c01a90c927b8ac20ccc9ffd9
Shuvendu Bikash <bikash11roy@gmail.com>
2018-01-06 15:29:27 +0600

    training the movie review classifier

--- classification/movie_review_binary_classification.py
+++ classification/movie_review_binary_classification.py
@@ -1,4 +1,6 @@
+"""Loading the data"""
 from keras.datasets import imdb
+
 (train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words=10000)
 
 word_index = imdb.get_word_index()
@@ -6,14 +8,53 @@ word_index = imdb.get_word_index()
 reverse_word_index = dict([(value, key) for (key, value) in word_index.items()])
 decoded_review = ' '.join([reverse_word_index.get(i - 3, '?') for i in train_data[0]])
 
+"""Data preprocessing"""
 import numpy as np
+
+
 def vectorize_sequences(sequences, dimension=10000):
     results = np.zeros((len(sequences), dimension))
     for i, sequence in enumerate(sequences):
         results[i, sequence] = 1.
     return results
+
+
 x_train = vectorize_sequences(train_data)
 x_test = vectorize_sequences(test_data)
 
 y_train = np.asarray(train_labels).astype('float32')
 y_test = np.asarray(test_labels).astype('float32')
+
+"""Building the network"""
+from keras import models
+from keras import layers
+
+model = models.Sequential()
+model.add(layers.Dense(16, activation='relu', input_shape=(10000,)))
+model.add(layers.Dense(16, activation='relu'))
+model.add(layers.Dense(1, activation='sigmoid'))
+
+# Compiling the model
+# model.compile(optimizer='rmsprop', loss=='binary_crossentropy', metrics=['accuracy'])
+from keras import losses
+from keras import optimizers
+from keras import metrics
+
+model.compile(optimizer=optimizers.RMSprop(lr=0.001),
+              loss=losses.binary_crossentropy,
+              metrics=[metrics.binary_accuracy])
+
+"""Training the model"""
+x_val = x_train[:10000]
+partial_x_train = x_train[10000:]
+
+y_val = y_train[:10000]
+partial_y_train = y_train[10000:]
+
+history = model.fit(partial_x_train,
+                    partial_y_train,
+                    epochs=20,
+                    batch_size=512,
+                    validation_data=(x_val, y_val))
+
+
