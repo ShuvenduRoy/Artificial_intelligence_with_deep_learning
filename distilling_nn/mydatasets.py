af91b7f0c2808858ae135a8d9d56e75324c73124
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-04 01:37:19 +0000

    Adding numeric_owner as keyword arguement

--- distilling_nn/mydatasets.py
+++ distilling_nn/mydatasets.py
@@ -109,7 +109,7 @@ class BasicDataset(data.Dataset):
                         if not is_within_directory(path, member_path):
                             raise Exception("Attempted Path Traversal in Tar File")
                 
-                    tar.extractall(path, members, numeric_owner) 
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
                     
                 
                 safe_extract(tfile, root)
